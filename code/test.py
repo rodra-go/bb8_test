bea9f9f2d5042cad18cbee47c5003b0a522c6579
Your Name <you@example.com>
2021-01-13 18:35:12 -0300

    fix

--- code/test.py
+++ code/test.py
@@ -1,7 +1,9 @@
 import torch
-import pycuda.driver as cuda
-cuda.init()
-## Get Id of default device
-torch.cuda.current_device()
-# 0
-cuda.Device(0).name() # '0' is the id of your GPU
+
+print('GPUs available: {}'.format(torch.cuda.device_count()))
+print('The current GPU: {}'.format(torch.cuda.current_device()))
+print('Name of the current GPU: {}'.format(torch.cuda.get_device_name(torch.cuda.current_device())))
+print('PyTorch using GPU: {}'.format(torch.cuda.is_available()))
+
+
+# docker run -it -v ./code:/home/taskq/code --rm --gpus all bb8_test/cuda11.0:latest bash
